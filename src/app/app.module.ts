adb1e1d683403b19305170b20655f19ff1495b28
Adam Foerster <adam.foerster@protonmail.com>
2020-01-22 22:19:30 -0200

    some scaffolding in prep for real reqs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -29,7 +30,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
